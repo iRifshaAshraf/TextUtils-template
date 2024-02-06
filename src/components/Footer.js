c304b7ab009a987f3fa889b0d3286e1bc7470d6d
Rifsha <rifsha.ashraf1@gmail.com>
2024-02-06 06:38:39 +0500

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,8 @@ export default function Footer(props) {
   return (
     <footer className={`bg-${props.mode} text-center pt-3 pb-1 mt-5`} style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
       <p>
-        &copy; 2024 TextUtils. All rights reserved. Developed by{' '}
+        &copy; 2024 TextUtils. All rights reserved. <br/>
+         Developed by{' '}
         <Link to={{ pathname: 'https://github.com/iRifshaAshraf' }} style={{ color: props.mode === 'dark' ? 'white' : 'black', textDecoration: 'none', fontWeight:'bold'}}>
           Rifsha Ashraf.
         </Link>
